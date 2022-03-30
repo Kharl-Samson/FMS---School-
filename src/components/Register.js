0f90d4fc8772162250b397cbe55a467542ecff04
xkhrlx <khrlsmsn1110@gmail.com>
2022-03-30 14:49:29 +0800

    Merge branch 'master' of https://github.com/xkhrlx/fms

--- src/components/Register.js
+++ src/components/Register.js
@@ -14,7 +14,14 @@ import Password_icon from "../images/icons/password.svg";
 import Open_eye_icon from "../images/icons/open_eye.svg";
 import Close_eye_icon from "../images/icons/close_eye.svg"; 
 import CICT_Logo from "../images/login/cict_logo.png";
-import Email_icon from "../images/icons/email.png"
+import Email_icon from "../images/icons/email.png";
+import Invalid_icon from "../images/icons/invalid.svg";
+import Valid_icon from "../images/icons/valid.svg";
+
+import validator from 'validator'
+import axios from "axios";
+import {useNavigate} from 'react-router-dom';
+import ModalValidation from "./ModalValidation";
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
@@ -73,11 +80,167 @@ export default function Register(){
         document.getElementsByClassName("close_eye")[1].style.display = "none"
     }
 
+  
+    function email_validation(){
+        if (validator.isEmail(document.getElementById("email").value)) {
+                document.getElementsByClassName("form_handler_container")[0].style.display = "flex";
+                document.getElementsByClassName("form_handler_container")[0].style.backgroundColor = "rgb(76, 180, 76)"
+                document.getElementsByClassName("img_verifyer")[0].src = Valid_icon;
+                document.getElementsByClassName("text_verifyer")[0].innerHTML = "Your email is valid.";
+                document.getElementsByClassName("email_container")[0].style.border = "1px solid #D8D8D8";   
+        } 
+        else if(document.getElementById("email").value === "" ){
+                document.getElementsByClassName("form_handler_container")[0].style.display = "none";
+                document.getElementsByClassName("form_handler_container")[0].style.backgroundColor = "rgb(76, 180, 76)"
+                document.getElementsByClassName("img_verifyer")[0].src = Valid_icon;
+                document.getElementsByClassName("text_verifyer")[0].innerHTML = "";
+                document.getElementsByClassName("email_container")[0].style.border = "1px solid #D8D8D8";   
+        }
+        else {
+                document.getElementsByClassName("form_handler_container")[0].style.display = "flex";
+                document.getElementsByClassName("text_verifyer")[0].innerHTML = "Your email is invalid.";
+                document.getElementsByClassName("form_handler_container")[0].style.backgroundColor = "#f7526d"
+                document.getElementsByClassName("img_verifyer")[0].src = Invalid_icon;
+                document.getElementsByClassName("email_container")[0].style.border = "1px solid red";   
+        }
+    }
+
+    function password_validation(){
+        if (validator.isStrongPassword(document.getElementById('password').value, {
+            minLength: 8, minLowercase: 1,
+            minUppercase: 1, minNumbers: 1, minSymbols: 1
+          })) {          
+                document.getElementsByClassName("form_handler_container")[0].style.display = "flex";
+                document.getElementsByClassName("form_handler_container")[0].style.backgroundColor = "rgb(76, 180, 76)"
+                document.getElementsByClassName("img_verifyer")[0].src = Valid_icon;
+                document.getElementsByClassName("text_verifyer")[0].innerHTML = "Your password is valid.";
+                document.getElementsByClassName("password_container")[0].style.border = "1px solid #D8D8D8";
+          } else {
+                document.getElementsByClassName("form_handler_container")[0].style.display = "flex";
+                document.getElementsByClassName("text_verifyer")[0].innerHTML = "Your password is not strong.";
+                document.getElementsByClassName("form_handler_container")[0].style.backgroundColor = "#f7526d"
+                document.getElementsByClassName("img_verifyer")[0].src = Invalid_icon;
+                document.getElementsByClassName("password_container")[0].style.border = "1px solid red";      
+          }
+          
+          if(document.getElementById("password").value === "" ){
+                document.getElementsByClassName("form_handler_container")[0].style.display = "none";
+                document.getElementsByClassName("text_verifyer")[0].innerHTML = "";
+                document.getElementsByClassName("form_handler_container")[0].style.backgroundColor = "#f7526d"
+                document.getElementsByClassName("img_verifyer")[0].src = Invalid_icon;
+                document.getElementsByClassName("password_container")[0].style.border = "1px solid #D8D8D8";
+                document.getElementsByClassName("confirm_password_container")[0].style.border = "1px solid #D8D8D8";   
+          }
+    }
+
+    function password_verifyer(){
+        const pass = document.getElementById('password').value;
+        const confirm_pass = document.getElementById('confirm_password').value;
+
+            if(pass !== confirm_pass && confirm_pass !== ""){
+                document.getElementsByClassName("form_handler_container")[0].style.display = "flex";
+                document.getElementsByClassName("text_verifyer")[0].innerHTML = "Please make sure your password match.";
+                document.getElementsByClassName("form_handler_container")[0].style.backgroundColor = "#f7526d"
+                document.getElementsByClassName("img_verifyer")[0].src = Invalid_icon;
+                document.getElementsByClassName("confirm_password_container")[0].style.border = "1px solid red";   
+            }
+            else if(confirm_pass === ""){
+                document.getElementsByClassName("password_container")[0].style.border = "1px solid #D8D8D8"; 
+                document.getElementsByClassName("confirm_password_container")[0].style.border = "1px solid #D8D8D8";   
+                document.getElementsByClassName("form_handler_container")[0].style.display = "none";
+                document.getElementsByClassName("text_verifyer")[0].innerHTML = "";
+                document.getElementsByClassName("form_handler_container")[0].style.backgroundColor = "#f7526d"
+                document.getElementsByClassName("img_verifyer")[0].src = Invalid_icon;  
+            }
+            else{
+                document.getElementsByClassName("form_handler_container")[0].style.display = "none";
+                document.getElementsByClassName("text_verifyer")[0].innerHTML = "";
+                document.getElementsByClassName("form_handler_container")[0].style.backgroundColor = "#f7526d"
+                document.getElementsByClassName("img_verifyer")[0].src = Invalid_icon;  
+                document.getElementsByClassName("confirm_password_container")[0].style.border = "1px solid #D8D8D8";   
+            }
+    }
+
+    let history = useNavigate();
+    const [data,setData ] = useState({
+        first_name:"",
+        last_name:"",
+        email:"",
+        password:"",
+        confirm_password:""
+    })
+
+    const handleChange=(e)=>{
+        setData({ ...data, [e.target.name]: e.target.value });
+    }
+
+    const submitForm=(e)=>{
+        e.preventDefault();
+
+        const pass = document.getElementById('password').value;
+        const confirm_pass = document.getElementById('confirm_password').value;
+
+        const sendData = {
+            first_name:data.first_name,
+            last_name:data.last_name,
+            email:data.email,
+            password:data.password,
+            confirm_password:data.confirm_password
+        }
+        console.log(sendData)
+
+        if (validator.isEmail(document.getElementById("email").value)) {
+            if (validator.isStrongPassword(document.getElementById('password').value, {
+                minLength: 8, minLowercase: 1,
+                minUppercase: 1, minNumbers: 1, minSymbols: 1
+            })) {  
+
+                axios.post('http://localhost/fms/register.php',sendData)
+                .then((result)=>{
+                    
+                    if(result.data.status == "Invalid"){
+                        alert('Invalid User')
+                    }
+                    else if(result.data.status == "Password doesnt match"){
+                        document.getElementsByClassName("form_handler_container")[0].style.display = "flex";
+                        document.getElementsByClassName("text_verifyer")[0].innerHTML = "Please make sure your password match.";
+                        document.getElementsByClassName("form_handler_container")[0].style.backgroundColor = "#f7526d"
+                        document.getElementsByClassName("img_verifyer")[0].src = Invalid_icon;
+                        document.getElementsByClassName("confirm_password_container")[0].style.border = "1px solid red";   
+                    }
+                    else if(result.data.status == "Email taken"){
+                        document.getElementsByClassName("form_handler_container")[0].style.display = "flex";
+                        document.getElementsByClassName("text_verifyer")[0].innerHTML = "E-mail has already been taken.";
+                        document.getElementsByClassName("form_handler_container")[0].style.backgroundColor = "#f7526d"
+                        document.getElementsByClassName("img_verifyer")[0].src = Invalid_icon;
+                        document.getElementsByClassName("email_container")[0].style.border = "1px solid red";   
+                    }
+                    else if(result.data.status == "valid"){ 
+                        //pag tama lalabas modal
+                        document.getElementsByClassName("modal_container")[0].style.display = "flex";
+                    }
+    
+                })//ito
+            }
+            else {
+                document.getElementsByClassName("form_handler_container")[0].style.display = "flex";
+                document.getElementsByClassName("text_verifyer")[0].innerHTML = "Your password is not strong.";
+                document.getElementsByClassName("form_handler_container")[0].style.backgroundColor = "#f7526d"
+                document.getElementsByClassName("img_verifyer")[0].src = Invalid_icon;
+                document.getElementsByClassName("password_container")[0].style.border = "1px solid red";    
+            }
+        }
+        else{
+            document.getElementsByClassName("form_handler_container")[0].style.display = "flex";
+            document.getElementsByClassName("text_verifyer")[0].innerHTML = "Your email is invalid.";
+            document.getElementsByClassName("form_handler_container")[0].style.backgroundColor = "#f7526d"
+            document.getElementsByClassName("img_verifyer")[0].src = Invalid_icon;
+            document.getElementsByClassName("email_container")[0].style.border = "1px solid red";   
+        }
+    }
 
     return (
         <div className="login_container">
-
-
             <Link to="/Home" style={{ textDecoration: 'none' }}>
                 <div className="home_container_btn">
                     <img src={Home_Icon}/>
@@ -100,6 +263,7 @@ export default function Register(){
             </div>
 
             <div className="login_form_container register_form_container">
+                <form onSubmit={submitForm}>
                 <Container maxWidth="md">
                     <Box sx={{  height: 'auto' }}>
 
@@ -116,32 +280,32 @@ export default function Register(){
                                     </Link>
                                 </p>
 
-                                <div className="input_container input_container1">
+                                <div className="input_container input_container1 fname_container">
                                     <div className="icon_container">
                                             <img src={Username_icon} className=".for_hover" title="First Name"/>
                                     </div>
-                                    <input type="text" placeholder="First Name" />
+                                    <input type="text" placeholder="First Name" name="first_name" onChange={handleChange} value={data.first_name} required/>
                                 </div>
 
-                                <div className="input_container input_container2">
+                                <div className="input_container input_container2 lname_container">
                                     <div className="icon_container">
                                             <img src={Username_icon} className=".for_hover" title="Last Name"/>
                                     </div>
-                                    <input type="text" placeholder="Last Name" />
+                                    <input type="text" placeholder="Last Name" name="last_name" onChange={handleChange} value={data.last_name} required/>
                                 </div>
 
-                                <div className="input_container input_container2">
+                                <div className="input_container input_container2 email_container">
                                     <div className="icon_container">
                                             <img src={Email_icon} className=".for_hover" title="Email"/>
                                     </div>
-                                    <input type="text" placeholder="Email" />
+                                    <input type="text" placeholder="Email" name="email" id="email" onChange={handleChange} onKeyUp={email_validation} value={data.email} required/>
                                 </div>
 
-                                <div className="input_container input_container2">
+                                <div className="input_container input_container2 password_container">
                                     <div className="icon_container">
                                             <img src={Password_icon} title="Password"/>
                                     </div>
-                                    <input type="password" placeholder="Password" id="password" onKeyUp={TogglePass}/>
+                                    <input type="password" placeholder="Password" name="password"  id="password" onKeyUp={() => { TogglePass(); password_validation();}} onChange={handleChange} value={data.password} required/>
                                     <div className="toggle_password">                           
                                             <img src={Open_eye_icon} 
                                                 className="password_toggle_icon open_eye"
@@ -158,11 +322,11 @@ export default function Register(){
                                     </div>
                                 </div>
 
-                                <div className="input_container input_container2">
+                                <div className="input_container input_container2 confirm_password_container">
                                     <div className="icon_container">
                                             <img src={Password_icon} title="Confirm Password"/>
                                     </div>
-                                    <input type="password" placeholder="Confirm Password" id="confirm_password" onKeyUp={TogglePass1}/>
+                                    <input type="password" placeholder="Confirm Password" name="confirm_password" id="confirm_password" onKeyUp={() => { TogglePass1(); password_verifyer();}}  onChange={handleChange} value={data.confirm_password} required/>
                                     <div className="toggle_password">                           
                                             <img src={Open_eye_icon} 
                                                 className="password_toggle_icon open_eye"
@@ -179,7 +343,12 @@ export default function Register(){
                                     </div>
                                 </div>
 
-                                <button className="sign_up_btn">Sign up</button>
+                                <div className="form_handler_container">
+                                     <img src={Invalid_icon} className="img_verifyer"/>
+                                     <p className="text_verifyer"></p>
+                                </div>          
+
+                                <button type="submit" className="sign_up_btn">Sign up</button>
            
                                 <p className="terms_and_service">
                                     By continuing to use this website, I state that I have read and understood the <span>Terms of Service</span> and <span>Privacy Policy</span>.
@@ -195,6 +364,7 @@ export default function Register(){
 
                     </Box>
                 </Container>
+                </form>
             </div>
 
             <div className="footer">
@@ -209,6 +379,17 @@ export default function Register(){
                     <span>CICT © 2022</span>
                 </Grid>                                                    
             </div>
+
+            {/*Modal*/ }
+            <div className="modal_container">
+                    <ModalValidation
+                        name = "EmailValidation.gif"
+                        headtext = "Successful!"
+                        subtext = "Your submission has been sent! Kindly check your email and spam for the reply. Thanks!"
+                        button1 = "Close"
+                    />
+            </div>
+
 
         </div>
     )
