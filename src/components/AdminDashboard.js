7bce710e82774bb6c6dbfc6755d5e79f51fe3f43
xkhrlx <khrlsmsn1110@gmail.com>
2022-05-06 11:48:14 +0800

    admin

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -51,12 +51,16 @@ export default function AdminDashboard(){
                         <div className='left'>
                             <span>Faculty</span>
                             <AvatarGroup max={4}>
-                                <Avatar alt="Remy Sharp" src="http://localhost/fms/upload_certificate/personA.jpg" sx={{ width: "30px", height: "30px" ,fontSize:".9rem"}}/>
-                                <Avatar alt="Travis Howard" src="http://localhost/fms/upload_certificate/personb.jpg" sx={{ width: "30px", height: "30px",fontSize:".9rem" }}/>
-                                <Avatar alt="Cindy Baker" src="http://localhost/fms/upload_certificate/personC.jpg" sx={{ width: "30px", height: "30px",fontSize:".9rem" }}/>
-                                <Avatar alt="Agnes Walker" src="http://localhost/fms/upload_certificate/e.png" sx={{ width: "30px", height: "30px" ,fontSize:".9rem"}} />
-                                <Avatar alt="Trevor Henderson" src="http://localhost/fms/upload_certificate/f.png" sx={{ width: "30px", height: "30px",fontSize:".9rem" }}/>
+                                <Avatar alt="Remy Sharp" src="http://localhost/fms/upload_certificate/personA.jpg" sx={{ width: "32px", height: "32px" ,fontSize:".9rem"}}/>
+                                <Avatar alt="Travis Howard" src="http://localhost/fms/upload_certificate/personb.jpg" sx={{ width: "32px", height: "32px",fontSize:".9rem" }}/>
+                                <Avatar alt="Cindy Baker" src="http://localhost/fms/upload_certificate/personC.jpg" sx={{ width: "32px", height: "32px",fontSize:".9rem" }}/>
+                                <Avatar alt="Agnes Walker" src="http://localhost/fms/upload_certificate/e.png" sx={{ width: "32px", height: "32px" ,fontSize:".9rem"}} />
+                                <Avatar alt="Trevor Henderson" src="http://localhost/fms/upload_certificate/f.png" sx={{ width: "32px", height: "32px",fontSize:".9rem" }}/>
                             </AvatarGroup>
+                        </div>
+
+                        <div className='right'>
+                            <div className='close_btn'><span title="Close">&#187;</span></div>
                         </div>
                     </div>
                 </div>
